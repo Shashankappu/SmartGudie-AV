7cbac537a3fc9039bdacf782b8339d0ca6de54b8
shashank <shashankprince523@gmail.com>
2024-02-27 11:44:41 +0530

    logo n app icon changed

--- app/src/main/java/com/shashankssp/smartguides/ui/screens/HomeActivity.kt
+++ app/src/main/java/com/shashankssp/smartguides/ui/screens/HomeActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
